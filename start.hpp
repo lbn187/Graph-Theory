292ae8715cddefd565c502628b98d147962d5bb6
lbn187 <36894888+lbn187@users.noreply.github.com>
2018-07-02 01:08:35 +0800

    Create start.hpp

---
+++ start.hpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define N 11111
+#define PB push_back
+#define MP make_pair
+#define PII pair<int,int>
+#define VI vector<int>
+#define fr(i,n) for(int i=1;i<=n;i++)
+#define X first
+#define Y second
+#define inf 1e18
+#define rd(x) scanf("%d",&x)
+#define LL long long
+using namespace std;
