e06901986294ffe1eac7cc32ea61e5b2f0b5d3ee
Boring Lee <36894888+lbn187@users.noreply.github.com>
2018-07-03 03:08:15 +0800

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -4,8 +4,12 @@
 //vector<int> V[N] - The vector to store edges
 //Graph() - Make a graph with no vertexs or edges
 //Graph(n) - Make a graph with no vertexs and no edges
-//Graph(G) - Mkae the graph by copying the graph G
+//Graph(G) - Make the graph by copying the graph G
 //Graph &operator=(const Graph&G) - Assignment the graph from G 
+//void clear() - Clear all the edges
+//void ins(int x,int y) - Add a edge from vertex x to vertex y
+//void BFS(Graph G, int S) - Get BFS order from vertex S
+//void Topological_Sorting(Graph G) - Make topological sorting
 //Graph Anti_Graph(Graph G) - Get the anti-graph of G
 #include "start.hpp"
 struct Graph{
@@ -20,15 +24,59 @@ struct Graph{
 		n=G.n;m=G.m;
 		fr(i,n)V[i]=G.V[i];
 	}
-	~Graph(){fr(i,n)V[i].clear();}
+	~Graph(){clear();}
 	Graph &operator=(const Graph &G){
 		if(this==&G)return *this;
 		fr(i,n)V[i].clear();
 		n=G.n;m=G.m;
 		fr(i,n)V[i]=G.V[i];
 	}
-	void ins(int x,int y){V[x].PB(y);m++;}//加入一条从x到y权值为z的边
+	void clear(){fr(i,n)V[i].clear();m=0;}
+	void ins(int x,int y){V[x].PB(y);m++;}
+	/*
+	VI BFS(int S){
+		VI E;queue<int>Q;bool v[N];
+		fr(i,n)v[i]=0;
+		Q.push(S);v[S]=1;
+		for(;!Q.empty();){
+			int x=Q.front();E.PB(x);Q.pop();
+			for(int y:V[x])if(!v[y])v[y]=1,Q.push(y);
+		}
+		return E;
+	}
+	VI Topological_Sorting(){
+		int du[N];VI E;queue<int>Q; 
+		fr(i,n)du[i]=0;
+		fr(x,n)for(int y:V[x])du[y]++;
+		fr(i,n)if(!du[i])Q.push(i);
+		for(;!Q.empty();){
+			int x=Q.front();E.PB(x);Q.pop();
+			for(int y:V[x])if(!--du[y])Q.push(y);
+		}
+		return E;
+	}*/
 };
+VI BFS(Graph G,int S){
+	VI E;queue<int>Q;bool v[N];
+	fr(i,G.n)v[i]=0;
+	Q.push(S);v[S]=1;
+	for(;!Q.empty();){
+		int x=Q.front();E.PB(x);Q.pop();
+		for(int y:G.V[x])if(!v[y])v[y]=1,Q.push(y);
+	}
+	return E;
+}
+VI Topological_Sorting(Graph G){
+	int du[N];VI E;queue<int>Q; 
+	fr(i,G.n)du[i]=0;
+	fr(x,G.n)for(int y:G.V[x])du[y]++;
+	fr(i,G.n)if(!du[i])Q.push(i);
+	for(;!Q.empty();){
+		int x=Q.front();E.PB(x);Q.pop();
+		for(int y:G.V[x])if(!--du[y])Q.push(y);
+	}
+	return E;
+}
 Graph Anti_Graph(Graph G){
 	bool *vs;
 	vs=new bool[G.n+1];
