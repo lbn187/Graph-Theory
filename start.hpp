e6fada57f5006ae415f06d93962b533549f1413e
Boring Lee <36894888+lbn187@users.noreply.github.com>
2018-07-09 17:52:17 +0800

    Update start.hpp

--- start.hpp
+++ start.hpp
@@ -1,16 +1,26 @@
-#ifndef START_HPP
-#define START_HPP
 #include<bits/stdc++.h>
-#define PB push_back
+#define eps 1e-8
+#define pi 3.14159265358979
+#define LL long long
 #define MP make_pair
+#define PH push
+#define PB push_back
 #define PII pair<int,int>
 #define VI vector<int>
-#define fr(i,n) for(int i=1;i<=n;i++)
 #define X first
 #define Y second
+#define Max(a,b) a=max(a,b)
+#define Min(a,b) a=min(a,b)
+#define sqr(x) ((x)*(x))
+#define CL(a) memset(a,0,sizeof a)
+#define fr(i,n) for(int i=1;i<=n;i++)
+#define FR(i,S,n) for(int i=S;i<=n;i++)
+#define dr(i,n) for(int i=n;i;i--)
+#define DR(i,n,S) for(int i=n;i>=S;i--)
+#define SZ(x) ((int)(x).size())
 #define rd(x) scanf("%d",&x)
-#define LL long long
-using namespace std;
+#define pr(x) printf("%d",x)
 const int N=111111;
+const int M=1000000007;
 const int inf=1e9;
-#endif
+using namespaces std;
