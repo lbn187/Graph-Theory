4c2722c22deac8919cc848e6e0ecade57854af6b
Boring Lee <36894888+lbn187@users.noreply.github.com>
2018-07-11 00:49:32 +0800

    Update Bipartite_Graph.hpp

--- Bipartite_Graph.hpp
+++ Bipartite_Graph.hpp
@@ -34,7 +34,7 @@ struct Bipartite_Graph:public Graph{
 		}
 		bool HKbfs(){
 			fr(i,ln)dl[i]=0;fr(i,rn)dr[i]=0;
-			queue<int>Q;int dis=ln+rn+2;
+			QI Q;int dis=ln+rn+2;
 			fr(i,ln)if(!lkx[i])Q.push(i);
 			for(;!Q.empty();){
 				int x=Q.front();Q.pop();
@@ -60,12 +60,11 @@ struct Bipartite_Graph:public Graph{
 			fr(i,ln)if(!lkx[i])Scheme_dfs(i);
 		}
 	public:
-		Bipartite_Graph(int _ln,int _rn):ln(_ln),rn(_rn),m(0){}
+		Bipartite_Graph(int _ln,int _rn):ln(_ln),rn(_rn),m(0),Graph(_ln){}
 		Bipartite_Graph(Graph _G,int _ln,int _rn):ln(_ln),rn(_rn),m(_G.m){Graph::operator=(_G);}
 		void ins(int x,int y){Graph::ins(x,y);m++;}
 		int Hungary(){
-			int an=0;
-			start();
+			int an=0;start();
 			fr(i,ln)if(Hungary_Find(i,i))an++;
 			return an;
 		}
@@ -76,19 +75,15 @@ struct Bipartite_Graph:public Graph{
 				fr(i,ln)if(!lkx[i]&&HKdfs(i,++TM))an++;
 			return an;
 		}
-		void print(){
-			fr(i,rn)if(lky[i])printf("%d - %d\n",lky[i],i);
-		}
+		void print(){fr(i,rn)if(lky[i])printf("%d - %d\n",lky[i],i);}
 		pair<VI,VI> Minimum_Vertex_Coverage(){
-			Scheme_Find();
-			VI VL,VR;
+			VI VL,VR;Scheme_Find();
 			fr(i,ln)if(lkx[i]&&!vl[i])VL.PB(i);
 			fr(i,rn)if(vr[i])VR.PB(i);
 			return MP(VL,VR);
 		}
 		pair<VI,VI> Largest_Independent_Set(){
-			Scheme_Find();
-			VI VL,VR;
+			VI VL,VR;Scheme_Find();
 			fr(i,ln)if(!lkx[i]||vl[i])VL.PB(i);
 			fr(i,rn)if(!vr[i])VR.PB(i);
 			return MP(VL,VR);
@@ -105,93 +100,4 @@ int Minimum_Path_Coverage(Graph _G){
 	fr(x,_G.n)for(int y:_G.V[x])G.ins(x,y);
 	return _G.n-G.Hungary();
 }
-/*
-struct Bipartite_Graph{
-	static const int N=1111;
-	Graph G;int ln,rn,m,lky[N],lkx[N];
-	private:
-		int v[N],dl[N],dr[N];bool vl[N],vr[N];
-		void start(){
-			fr(i,rn)lky[i]=v[i]=0;
-			fr(i,ln)lkx[i]=0;
-		}
-		bool Hungary_Find(int x,int z){
-			for(int y:G.V[x])if(v[y]!=z)
-				if(v[y]=z,!lky[y]||Hungary_Find(lky[y],z))
-					return lky[y]=x,lkx[x]=y,1;
-			return 0;
-		}
-		bool HKbfs(){
-			fr(i,ln)dl[i]=0;fr(i,rn)dr[i]=0;
-			queue<int>Q;int dis=ln+rn+2;
-			fr(i,ln)if(!lkx[i])Q.push(i);
-			for(;!Q.empty();){
-				int x=Q.front();Q.pop();
-				if(dl[x]>dis)continue;
-				for(int y:G.V[x])if(!dr[y])
-					if(dr[y]=dl[x]+1,lky[y])dl[lky[y]]=dr[y]+1,Q.push(lky[y]);
-					else dis=dr[y];
-			}
-			return dis<ln+rn+2;
-		}
-		bool HKdfs(int x,int z){
-			for(int y:G.V[x])if(v[y]!=z&&dr[y]==dl[x]+1)
-				if(v[y]=z,!lky[y]||HKdfs(lky[y],z))
-					return lkx[x]=y,lky[y]=x,1;
-			return 0;
-		}
-		void Scheme_dfs(int x){
-			vl[x]=1;
-			for(int y:G.V[x])if(!vr[y]&&lky[y])vr[y]=1,Scheme_dfs(lky[y]);
-		}
-		void Scheme_Find(){
-			fr(i,ln)vl[i]=0;fr(i,rn)vr[i]=0;
-			fr(i,ln)if(!lkx[i])Scheme_dfs(i);
-		}
-	public:
-		Bipartite_Graph(int _ln,int _rn):ln(_ln),rn(_rn),m(0){}
-		Bipartite_Graph(Graph _G,int _ln,int _rn):ln(_ln),rn(_rn){G=_G;m=G.m;}
-		void ins(int x,int y){G.ins(x,y);m++;}
-		int Hungary(){
-			int an=0;
-			start();
-			fr(i,ln)if(Hungary_Find(i,i))an++;
-			return an;
-		}
-		int Hopcroft_Karp(){
-			int an=0,TM=0;
-			fr(i,ln)lkx[i]=0;fr(i,rn)lky[i]=0;
-			for(;an<min(ln,rn)&&HKbfs();)
-				fr(i,ln)if(!lkx[i]&&HKdfs(i,++TM))an++;
-			return an;
-		}
-		void print(){
-			fr(i,rn)if(lky[i])printf("%d - %d\n",lky[i],i);
-		}
-		pair<VI,VI> Minimum_Vertex_Coverage(){
-			Scheme_Find();
-			VI VL,VR;
-			fr(i,ln)if(lkx[i]&&!vl[i])VL.PB(i);
-			fr(i,rn)if(vr[i])VR.PB(i);
-			return MP(VL,VR);
-		}
-		pair<VI,VI> Largest_Independent_Set(){
-			Scheme_Find();
-			VI VL,VR;
-			fr(i,ln)if(!lkx[i]||vl[i])VL.PB(i);
-			fr(i,rn)if(!vr[i])VR.PB(i);
-			return MP(VL,VR);
-		}
-		pair<VI,VI> Maximum_Clique(){
-			Bipartite_Graph W(Anti_Graph(G),ln,rn);
-			return W.Largest_Independent_Set();
-		}
-		pair<VI,VI> Essential_Vertex(){//have not finished yet
-		}
-};
-int Minimum_Path_Coverage(Graph _G){
-	Bipartite_Graph G(_G.n,_G.n);
-	fr(x,_G.n)for(int y:_G.V[x])G.ins(x,y);
-	return _G.n-G.Hungary();
-}*/
 #endif
