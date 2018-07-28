a0b597d5b211984cd2449bd4db47f8897c513216
Boring Lee <36894888+lbn187@users.noreply.github.com>
2018-07-29 02:31:02 +0800

    Update Weight_Graph.hpp

--- Weight_Graph.hpp
+++ Weight_Graph.hpp
@@ -10,20 +10,41 @@
 #ifndef WEIGHT_GRAPH_HPP
 #define WEIGHT_GRAPH_HPP
 #include "start.hpp"
-template<typename T,const int N> struct Weight_Graph{
+#include "Graph.hpp"
+template<typename T=LL,const int N=111111>struct Weight_Graph{
 	int n,m;
 	vector<pair<int,T> >V[N];
 	Weight_Graph()=default;
 	Weight_Graph(int _n=0):n(_n),m(0){}
 	Weight_Graph(const Weight_Graph&G):n(G.n),m(G.m){fr(i,n)V[i]=G.V[i];}
-	~Weight_Graph(){fr(i,n)V[i].clear();}
+	~Weight_Graph(){clear();}
 	Weight_Graph &operator=(const Weight_Graph&G){
 		if(this==&G)return *this;
 		fr(i,n)V[i].clear();
 		n=G.n;m=G.m;
 		fr(i,n)V[i]=G.V[i];
 	}
+	Weight_Graph(Weight_Graph &&G)noexcept:n(G.n),m(G.m){fr(i,n)V[i].swap(G.V[i]);}
+	Weight_Graph &operator=(Weight_Graph &&G)noexcept{
+		n=G.n;m=G.m;
+		fr(i,n)V[i].swap(G.V[i]);
+		return *this;
+	}
 	void clear(){fr(i,n)V[i].clear();m=0;}
 	void ins(int x,int y,T z){V[x].PB(MP(y,z));m++;}
+	Graph To_Graph();
 };
+template<typename T,const int N>
+ostream &operator<<(ostream &os,const Weight_Graph<T,N> &G){
+	os<<"N = "<<G.n<<" , "<<"M = "<<G.m<<endl;
+	fr(x,G.n)for(auto o:G.V[x])os<<x<<" -> "<<o.X<<" ["<<o.Y<<"]"<<endl;
+	return os;
+}
+template<typename T=LL,const int N=111111>
+istream &operator>>(istream &is,Weight_Graph<T,N> &G){
+	is>>G.n>>G.m;
+	int x,y;T z;
+	fr(i,G.m)is>>x>>y>>z,G.ins(x,y,z);
+	return is;
+}
 #endif
