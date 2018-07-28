845ae46093995fc9e018350d806fe801f4c046d7
Boring Lee <36894888+lbn187@users.noreply.github.com>
2018-07-28 17:49:39 +0800

    Update Weight_Graph.hpp

--- Weight_Graph.hpp
+++ Weight_Graph.hpp
@@ -10,15 +10,14 @@
 #ifndef WEIGHT_GRAPH_HPP
 #define WEIGHT_GRAPH_HPP
 #include "start.hpp"
-template<typename T> struct Weight_Graph{
-	static const int N=111111;
+template<typename T,const int N> struct Weight_Graph{
 	int n,m;
 	vector<pair<int,T> >V[N];
 	Weight_Graph()=default;
-	Weight_Graph(int _n=0):n(_n),m(0){fr(i,n)V[i].clear();}
-	Weight_Graph(const Weight_Graph<T>&G):n(G.n),m(G.m){fr(i,n)V[i]=G.V[i];}
+	Weight_Graph(int _n=0):n(_n),m(0){}
+	Weight_Graph(const Weight_Graph&G):n(G.n),m(G.m){fr(i,n)V[i]=G.V[i];}
 	~Weight_Graph(){fr(i,n)V[i].clear();}
-	Weight_Graph &operator=(const Weight_Graph<T>&G){
+	Weight_Graph &operator=(const Weight_Graph&G){
 		if(this==&G)return *this;
 		fr(i,n)V[i].clear();
 		n=G.n;m=G.m;
