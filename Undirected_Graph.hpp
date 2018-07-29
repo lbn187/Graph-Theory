cc35801e68023c96b6dc966d6f62fe8d4fcf0d91
Boring Lee <36894888+lbn187@users.noreply.github.com>
2018-07-30 01:04:50 +0800

    Update Undirected_Graph.hpp

--- Undirected_Graph.hpp
+++ Undirected_Graph.hpp
@@ -12,10 +12,12 @@
 #define UNDIRECTED_GRAPH_HPP
 #include "start.hpp"
 struct Undirected_Graph{
+	friend ostream &operator<<(ostream &,const Undirected_Graph&);
+	friend istream &operator>>(istream &,Undirected_Graph&);
 	static const int N=111111;
 	int n,m;
 	VI V[N];
-	Undirected_Graph(int _n=0):n(_n),m(0){fr(i,n)V[i].clear();}
+	Undirected_Graph(int _n=0):n(_n),m(0){}
 	Undirected_Graph(const Undirected_Graph &G):n(G.n),m(G.m){fr(i,n)V[i]=G.V[i];}
 	~Undirected_Graph(){clear();}
 	Undirected_Graph &operator=(const Undirected_Graph &G){
@@ -23,8 +25,26 @@ struct Undirected_Graph{
 		fr(i,n)V[i].clear();
 		n=G.n;m=G.m;
 		fr(i,n)V[i]=G.V[i];
+		return *this;
+	}
+	Undirected_Graph(Undirected_Graph &&G)noexcept:n(G.n),m(G.m){fr(i,n)V[i].swap(G.V[i]);}
+	Undirected_Graph &operator=(Undirected_Graph &&G)noexcept{
+		n=G.n;m=G.m;
+		fr(i,n)V[i].swap(G.V[i]);
+		return *this;
 	}
 	void clear(){fr(i,n)V[i].clear();m=0;}
 	void ins(int x,int y){V[x].PB(y);V[y].PB(x);m++;}
 };
+ostream &operator<<(ostream &os,const Undirected_Graph &G){
+	os<<"N = "<<G.n<<" , "<<"M = "<<G.m<<endl;
+	fr(x,G.n)for(auto y:G.V[x])if(x<=y)os<<x<<" --- "<<y<<endl;
+	return os;
+}
+istream &operator>>(istream &is,Undirected_Graph &G){
+	is>>G.n>>G.m;
+	int x,y;
+	fr(i,G.m)is>>x>>y,G.ins(x,y);
+	return is;
+}
 #endif
